7307c8c085e6453c10642a8b80dc8633f68fbb62
Luke Stephenson <lukestephenson0@gmail.com>
2016-07-11 13:43:30 -0400

    Add main files

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var Boxnumber = prompt("How many boxes would you like?");
+
+
+for (var i = 0; i < Boxnumber;i++){
+$(document).ready(function() {
+
+  $('#wrapper').append('<div class="divBox"></div>')
+
+
+
+});
+};
